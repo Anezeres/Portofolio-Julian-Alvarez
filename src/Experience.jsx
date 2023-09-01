ec346f3d64f005442e259a91d7b97dd59ee5c485
Anezeres <alvarez.julian@correounivalle.edu.co>
2023-09-01 13:28:39 -0500

    Mi primer commit del proyecto

---
+++ src/Experience.jsx
@@ -0,0 +1,33 @@
+import { OrbitControls } from "@react-three/drei";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+
+const Experience = () => {
+
+    const boxRef = useRef();
+
+    useFrame((state, delta)=>{
+        boxRef.current.rotation.x += 1 * delta;
+    })
+
+    return (
+        <>
+            <OrbitControls makeDefault />
+            <ambientLight intensity={0.3}/>
+            <directionalLight position={[10,10, 5]} intensity={2}/>
+
+            <mesh ref={boxRef}> 
+                <boxGeometry args={[1, 1, 1]} />
+                <meshStandardMaterial wireframe={false} position={[10, 10, 5]} color="blue" />
+            </mesh>
+        </>
+    )
+
+    
+    
+
+    
+
+}
+
+export default Experience;
